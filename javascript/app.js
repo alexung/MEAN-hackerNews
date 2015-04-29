8a1eaeab9dc3ead00dde7f2f3f2c4888398ede8e
Alex Ung <alexung.ucsb@gmail.com>
2015-04-29 14:22:30 -0700

    Create the form to add links.  Leave them not underlined if there's no link to be included

--- javascript/app.js
+++ javascript/app.js
@@ -11,4 +11,21 @@ app.controller('MainCtrl', ['$scope', function($scope){
       {title: 'post 5', upvotes: 4}
     ];
 
+    $scope.addPost = function() {
+      // below prevents a user from entering a blank post.  No forcing user to enter in a link, however.
+        if(!$scope.title || $scope.title === "") {
+          return;
+        }
+        $scope.posts.push({title: $scope.title,
+          link: $scope.link,
+          upvotes: 0});
+        // resets $scope.title to be empty after the fact
+        $scope.title = '';
+        $scope.link = '';
+    }
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
+
 }]);
