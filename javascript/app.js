33e2efc89c1704abcc579d44064e19c1839db82c
Alex Ung <alexung.ucsb@gmail.com>
2015-04-29 22:32:18 -0700

    substitute dummy posts with posts from factory

--- javascript/app.js
+++ javascript/app.js
@@ -4,20 +4,23 @@ app.factory('posts', [function() {
   var o = {
     posts: []
   };
+
   // we return the o object so that any other Angular module that cares to inject it, can
   return o;
 }]);
 
-app.controller('MainCtrl', ['$scope', function($scope){
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     $scope.test = 'Hello world!';
 
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
+
+    // $scope.posts = [
+    //   {title: 'post 1', upvotes: 5},
+    //   {title: 'post 2', upvotes: 2},
+    //   {title: 'post 3', upvotes: 15},
+    //   {title: 'post 4', upvotes: 9},
+    //   {title: 'post 5', upvotes: 4}
+    // ];
 
     $scope.addPost = function() {
       // below prevents a user from entering a blank post.  No forcing user to enter in a link, however.
