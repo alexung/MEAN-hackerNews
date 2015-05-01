fe9957210bc5e12c69ed52457af2175f428f4a77
Alex Ung <alexung.ucsb@gmail.com>
2015-04-30 19:38:15 -0700

    Adding comments works, but the text of the comment does not display

--- MEAN-hackerNews/public/javascripts/angularApp.js
+++ MEAN-hackerNews/public/javascripts/angularApp.js
@@ -29,22 +29,16 @@ app.factory('posts', ['$http', function($http) {
       return res.data;
     });
   };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
   // we return the o object so that any other Angular module that cares to inject it, can
   return o;
 }]);
 
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
-    $scope.test = 'Hello world!';
-
     $scope.posts = posts.posts;
-
-    // $scope.posts = [
-    //   {title: 'post 1', upvotes: 5},
-    //   {title: 'post 2', upvotes: 2},
-    //   {title: 'post 3', upvotes: 15},
-    //   {title: 'post 4', upvotes: 9},
-    //   {title: 'post 5', upvotes: 4}
-    // ];
 
     $scope.addPost = function() {
       // below prevents a user from entering a blank post.  No forcing user to enter in a link, however.
@@ -120,10 +114,11 @@ app.controller('PostsCtrl', [
     $scope.addComment = function(){
       if($scope.body === '') {return;}
 
-      $scope.post.comments.push({
+      posts.addComment(post._id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0
+      }).success(function(comment){
+        $scope.post.comments.push(comment);
       });
 
       $scope.body = '';
